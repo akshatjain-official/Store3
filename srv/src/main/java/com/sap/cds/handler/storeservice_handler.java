445206f38ae7f47ea8bd896f66872ef4397bc2e4
Akshat Jain <akshat.jain02@sap.com>
2025-03-11 11:06:43 +0530

    Destination Issue Resolved

--- srv/src/main/java/com/sap/cds/handler/storeservice_handler.java
+++ srv/src/main/java/com/sap/cds/handler/storeservice_handler.java
@@ -1,21 +1,25 @@
 package com.sap.cds.handler;
 
-import java.io.Console;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.Result;
+import com.sap.cds.services.EventContext;
 import com.sap.cds.services.cds.CdsReadEventContext;
+import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.cds.RemoteService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 
 import cds.gen.catalog_service.CatalogService_;
 import cds.gen.storeservice.Storeservice_;
+import cds.gen.storeservice.Localattachment_;
 import cds.gen.storeservice.Localbooks_;
+import cds.gen.storeservice.Notebooks_;
 
 @Component
 @ServiceName(Storeservice_.CDS_NAME)
@@ -23,9 +27,18 @@ public class storeservice_handler implements EventHandler{
     @Autowired
     @Qualifier(CatalogService_.CDS_NAME)
     RemoteService remoteService;
-    @On(entity = Localbooks_.CDS_NAME)
+    @On(entity = {Localbooks_.CDS_NAME,Localattachment_.CDS_NAME})
     Result readSuppliers(CdsReadEventContext context){
         System.out.println("SERVICE HANDLER IS RUNNING #102");
         return remoteService.run(context.getCqn());
     }
+
+    @Autowired
+    PersistenceService persistenceService;
+    @Before(event = {CqnService.EVENT_READ},entity= Notebooks_.CDS_NAME)
+    public void validation(EventContext context){
+        System.out.println("SERVICE HANDLER IS RUNNING #201");
+
+    }
+
 }
